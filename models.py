6272d29e87b0af454736b592e3fb57beadd0df35
ThiagoL159 <thiagolayber159@gmail.com>
2026-08-25 16:52:42 -0300

    css completo / funcionalidade do carrinho

--- models.py
+++ models.py
@@ -7,45 +7,49 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS produtos  (
                 nomedoproduto TEXT NOT NULL UNIQUE,
                 valordoproduto FLOAT NOT NULL
                 )""")
-# produtos = [
-#     ("Salgado", 10.00),
-#     ("Hamburguer", 9.00),
-#     ("Bacon Burguer", 14.00),
-#     ("X Burguer", 13.00),
-#     ("X Salada", 14.50),
-#     ("Egg Burguer", 11.50),
-#     ("Egg X Burguer", 15.00),
-#     ("Egg X Burguer Especial", 15.50),
-#     ("Egg Bacon", 15.00),
-#     ("X bacon", 16.00),
-#     ("Egg X Bacon", 17.00),
-#     ("Frango", 16.00),
-#     ("Frango Hamburguer", 17.50),
-#     ("X Frango", 17.50),
-#     ("Egg Frango", 17.50),
-#     ("Egg X Frango", 18.50),
-#     ("X Frango Bacon", 19.00),
-#     ("Egg X Frango Bacon", 21.00),
-#     ("X tudo", 24.00),
-#     ("Paladar", 27.00),
-#     ("Ovo", 3.00),
-#     ("Carne", 5.00),
-#     ("Frango", 6.00),
-#     ("aentimento de culpa da maconha", 3.00),
-#     ("Queijo", 5.00),
-#     ("Bacon", 5.00),
-#     ("Guaravita", 3.00),
-#     ("LATA", 7.00),
-#     ("refriKS", 5.00),
-#     ("2 Litros", 13.00),
-#     ("Guaraviton", 5.00),
-#     ("AguaCom", 3.50),
-#     ("AguaSem", 3.00)
-# ]
+produtos = [
+    ("Salgado", 10.00),
+    ("Hamburguer", 9.00),
+    ("Bacon Burguer", 14.00),
+    ("X Burguer", 13.00),
+    ("X Salada", 14.50),
+    ("Egg Burguer", 11.50),
+    ("Egg X Burguer", 15.00),
+    ("Egg X Burguer Especial", 15.50),
+    ("Egg Bacon", 15.00),
+    ("X bacon", 16.00),
+    ("Egg X Bacon", 17.00),
+    ("Frango", 16.00),
+    ("Frango Hamburguer", 17.50),
+    ("X Frango", 17.50),
+    ("Egg Frango", 17.50),
+    ("Egg X Frango", 18.50),
+    ("X Frango Bacon", 19.00),
+    ("Egg X Frango Bacon", 21.00),
+    ("X tudo", 24.00),
+    ("Paladar", 27.00),
+    ("+Ovo", 3.00),
+    ("+Carne", 5.00),
+    ("+Frango", 6.00),
+    ("+Presunto", 3.00),
+    ("+Queijo", 5.00),
+    ("+Bacon", 5.00),
+    ("Guaravita", 3.00),
+    ("LATA", 7.00),
+    ("refriKS", 5.00),
+    ("2 Litros", 13.00),
+    ("Guaraviton", 5.00),
+    ("AguaCom", 3.50),
+    ("AguaSem", 3.00),
+    ("EntregaGeral", 5.00),
+    ("EntregaMorro", 8.00),
+    ("Entregavilla", 10.00)
+]
 
-# cursor.executemany("""INSERT OR IGNORE INTO produtos
-#                 (nomedoproduto, valordoproduto) VALUES (?,?)
-#                 """, produtos)
+cursor.executemany("""INSERT OR IGNORE INTO produtos
+                (nomedoproduto, valordoproduto) VALUES (?,?)
+                """, produtos)
+conexao.commit()
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS pedidos  (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
